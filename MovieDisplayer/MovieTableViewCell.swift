6096b746d0d31b15ba02c2890636b2d33ee746d7
CTime95 <a.jesse3001@gmail.com>
2018-09-15 11:09:23 -0500

    Initial commit

---
+++ MovieDisplayer/MovieTableViewCell.swift
@@ -0,0 +1,87 @@
+//
+//  MovieTableViewCell.swift
+//  MovieDisplayer
+//
+//  Created by Morales, Angel (MX - Mexico) on 14/09/18.
+//  Copyright © 2018 TheKairuz. All rights reserved.
+//
+
+import UIKit
+
+class MovieTableViewCell: UITableViewCell {
+
+    @IBOutlet weak var movieTitleLabel: UILabel!
+    
+    @IBOutlet weak var generalInfoStack: UIStackView!
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        
+    }
+    
+    func makeButton() -> UIButton {
+        let hourButton = UIButton(type: .custom)
+        hourButton.setTitle("10:00am", for: .normal)
+        hourButton.titleLabel?.font = UIFont.systemFont(ofSize: 12.0)
+        hourButton.backgroundColor = UIColor.red
+        return hourButton
+    }
+    
+    func makeScheduleStack() -> UIStackView {
+        let scheduleStack = UIStackView()
+        scheduleStack.alignment = .fill
+        scheduleStack.distribution = .fillEqually
+        scheduleStack.axis = .horizontal
+        scheduleStack.spacing = 30
+        
+        let hourButton = makeButton()
+        let hourButton2 = makeButton()
+        let hourButton3 = makeButton()
+
+        scheduleStack.addArrangedSubview(hourButton)
+        scheduleStack.addArrangedSubview(hourButton2)
+        scheduleStack.addArrangedSubview(hourButton3)
+        
+        addSubview(scheduleStack)
+        scheduleStack.translatesAutoresizingMaskIntoConstraints = false
+        scheduleStack.heightAnchor.constraint(equalToConstant: 20).isActive = true
+
+        
+        return scheduleStack
+    }
+    
+    
+    func configure() {
+        
+        let salonStack = UIStackView()
+        salonStack.alignment = .fill
+        salonStack.distribution = .fill
+        salonStack.axis = .horizontal
+
+        let languageType = UIButton(type: .custom)
+        languageType.setTitle("SUB", for: .normal)
+        languageType.titleLabel?.font = UIFont.systemFont(ofSize: 12.0)
+        
+        languageType.backgroundColor = UIColor.red
+        generalInfoStack.addArrangedSubview(languageType)
+        languageType.translatesAutoresizingMaskIntoConstraints = false
+
+        let stack = makeScheduleStack()
+        generalInfoStack.addArrangedSubview(stack)
+        let stack2 = makeScheduleStack()
+        generalInfoStack.addArrangedSubview(stack2)
+        let stack3 = makeScheduleStack()
+        generalInfoStack.addArrangedSubview(stack3)
+        let stack4 = makeScheduleStack()
+        generalInfoStack.addArrangedSubview(stack4)
+        let stack5 = makeScheduleStack()
+        generalInfoStack.addArrangedSubview(stack5)
+        let stack6 = makeScheduleStack()
+        generalInfoStack.addArrangedSubview(stack6)
+       // contentView.layoutIfNeeded()
+
+    }
+  
+
+}
---
+++ MovieDisplayer/MovieViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  MovieDisplayer
+//
+//  Created by Morales, Angel (MX - Mexico) on 14/09/18.
+//  Copyright © 2018 TheKairuz. All rights reserved.
+//
+
+import UIKit
+
+class MovieViewController: UIViewController {
+
+    lazy var tableView : UITableView = {
+        let table = UITableView()
+        table.dataSource = self
+        table.delegate = self
+
+        let nib = UINib(nibName: "MovieCell", bundle: nil)
+        table.register(nib, forCellReuseIdentifier: "MovieCell")
+
+        return table
+        
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+       
+
+        view.addSubview(tableView)
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        tableView.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor).isActive = true
+        tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor).isActive = true
+        tableView.leftAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leftAnchor).isActive = true
+        tableView.rightAnchor.constraint(equalTo: view.safeAreaLayoutGuide.rightAnchor).isActive = true
+        
+        tableView.reloadData()
+    }
+    
+}
+
+extension MovieViewController : UITableViewDataSource, UITableViewDelegate{
+    
+    func numberOfSections(in tableView: UITableView) -> Int {
+        return 1
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 3
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "MovieCell", for: indexPath) as! MovieTableViewCell
+        cell.configure()
+        cell.layoutIfNeeded()
+        return cell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return UITableViewAutomaticDimension
+    }
+
+}
+
